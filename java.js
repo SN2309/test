b97a04d3302f139b11062d81846e07ada5ff53e8
SN2309 <shivamnema2309@gmail.com>
2023-02-23 15:22:09 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,9 @@
+<script>
+    window.Userback = window.Userback || {};
+    Userback.access_token = '39412|78333|SZ3V7Tn9nW7abMqDf52EdjdeK';
+    (function(d) {
+        var s = d.createElement('script');s.async = true;
+        s.src = 'https://static.userback.io/widget/v1.js';
+        (d.head || d.body).appendChild(s);
+    })(document);
+</script>
